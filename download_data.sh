ffe371448c4ef9696d98e516ad58e6b0054c5602
Filip Buric <29886463+fburic@users.noreply.github.com>
2024-02-29 10:23:43 +0100

    fix: Data and models download links

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget https://zenodo.org/records/10723375/files/data.zip
+wget https://zenodo.org/records/10725930/files/data.zip
 unzip data.zip
--- download_models.sh
+++ download_models.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget https://zenodo.org/records/10723375/files/model.zip
+wget https://zenodo.org/records/10725930/files/model.zip
 unzip model.zip
